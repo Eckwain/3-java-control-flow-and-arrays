0a9ef75aa78a8b6cb1c62a9971ec9b6003c16f15
Eckwain <falchemist049@gmail.com>
2025-09-21 22:38:58 +0500

    Solved Task13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,41 @@
 package com.example.task13;
 
+import java.util.Arrays;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        int[] filteredArray = removeMoreThen1000(arr);
+        System.out.println(Arrays.toString(filteredArray));
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        try {
+            int numOfMoreThan1000 = 0;
+            for (int j : arr){
+                if (j > 1000){
+                    numOfMoreThan1000++;
+                }
+            }
+            int[] newArr = new int[arr.length - numOfMoreThan1000];
+            int index = 0;
+            for (int i : arr){
+                if (i <= 1000){
+                    newArr[index] = i;
+                    index++;
+                }
+            }
+            return newArr;
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return null;
     }
 
 }
