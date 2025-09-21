fca25b2a79b20d9d6073a9345206c9181dabf779
Eckwain <falchemist049@gmail.com>
2025-09-21 22:24:52 +0500

    Solved Task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -13,6 +13,26 @@ public class Task12Main {
 
     static void selectionSort(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try{
+            for (int i = 0; i < arr.length; i++) {
+                int pos = i;
+                int min = arr[i];
+                //цикл выбора наименьшего элемента
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] < min) {
+                        //pos - индекс наименьшего элемента
+                        pos = j;
+                        min = arr[j];
+                    }
+                }
+                arr[pos] = arr[i];
+                //меняем местами наименьший с arr[i]
+                arr[i] = min;
+            }
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
