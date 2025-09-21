d1b3652b35c80636de77c555146dc057bcd9b31c
Eckwain <falchemist049@gmail.com>
2025-09-21 22:16:38 +0500

    Solved Task11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,41 @@
 package com.example.task11;
 
+import org.assertj.core.internal.bytebuddy.implementation.bytecode.Throw;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try {
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            for (int i = 0; i < arr.length; i++){
+                if (arr[i] <= min){
+                    min = arr[i];
+                    minIndex = i;
+                }
+            }
+            arr[minIndex] = arr[0];
+            arr[0] = min;
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+
+
     }
 
 }
